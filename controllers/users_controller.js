d7f521ec7defbee9f8586465b16b7a20e5d99b53
Yifeng_Z <yifengzhan@gmail.com>
2022-03-16 10:38:54 +1100

    Ethan user-controller

---
+++ controllers/users_controller.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+
+const router = express.Router();
+
+router.post("/", (req, res) => {
+  const { name, email, password } = req.body;
+
+  const passwordDigest = bcrypt.hashSync(
+    password,
+    bcrypt.genSaltSync(10),
+    null
+  );
+
+  User.create(name, email, passwordDigest).then((userName) =>
+    res.json(userName)
+  );
+});
+
+module.exports = router;
