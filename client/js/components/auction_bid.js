ccfb9e86e81fe48c6db599ed3122d334f8abef45
gordenyong <yongkunghee@hotmail.com>
2022-03-18 07:22:19 +0800

    merged

--- client/js/components/auction_bid.js
+++ client/js/components/auction_bid.js
@@ -1,36 +1,67 @@
 function renderAuction(data) {
-  console.log(state);
-  document.querySelector("#page").innerHTML = `
-    <div class="auction-info">
-      <div class="auction-left-side-info">
-        <img src="${data.image_url}" alt="">
-        <p>${data.item_description}</p>
-      </div>
+  console.log(data);
+  console.log(data.image_url);
+  if (state.userName) {
+    document.querySelector("#page").innerHTML = `
+    <section class="auction-bid">
+      <section>
+        <h2>${data.item_name}</h2>
+        <section>
+          <img class="bid-image" src=${data.image_url} alt=“”>
+          <div>
+            <h3>Time remaining:04:39</h3>
+            <h3>Current Bid: ${data.current_price}</h3>
+            <form action=“”>
+              <input type=“number” name=“placePrice” id=“placePriceId” value="${data.increment_price}" />
+              <button id=“increaseBidBtn”>Place Bid</button>
+            </form>
+          </div>
+        </section>
 
-      <div class="auction-right-side-info">
-        <div class="auction-data">
-          <h3>${data.item_name}</h3>
-        </div>
-
-        <div class="auction-data">
-          <span >Starting price:</span>
-          <span>${data.starting_price}</span>
-        </div>
-
-        <div class="auction-data">
-          <span >Increment price:</span>
-          <span>${data.increment_price}</span>
-        </div>
-        
-        <div class="auction-data">
-          <span >Current price:</span>
-          <span>${data.current_price}</span>
-        </div>
-
-        <button id="bid-btn">Increase Bid</button> 
-      </div>
-    </div>
+        <section>
+        <h3>Description:</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis
+          molestias pariatur, laboriosam nulla velit tempore quia harum eos
+          perferendis magni magnam iure, inventore veritatis illo ullam
+          consequuntur quam. Qui, voluptatibus.
+        </p>
+      </section>
+      <br><br>
+      <p>Auction id:${data.id}<p>
+    </section>
+   
     `;
+  } else {
+    document.querySelector("#page").innerHTML = `
+    <section class="auction-bid">
+      <h3 class="login-to-bid">Log In To PLace Bid</h3>
+      <section>
+        <h2>${data.item_name}</h2>
+        <section>
+          <img class="bid-image" src=${data.image_url} alt=“”>
+          <div>
+            <h3>Time remaining:04:39</h3>
+            <h3>Current Bid: ${data.current_price}</h3>
+            
+          </div>
+        </section>
+
+        <section class="bid-description">
+        <h3>Description:</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis
+          molestias pariatur, laboriosam nulla velit tempore quia harum eos
+          perferendis magni magnam iure, inventore veritatis illo ullam
+          consequuntur quam. Qui, voluptatibus.
+        </p>
+      </section>
+      <br><br>
+      <p>Auction id:${data.id}<p>
+    </section>
+   
+    `;
+  }
 }
 
 function renderAuctionBid(auction) {
