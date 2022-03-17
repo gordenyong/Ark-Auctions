74f809c0aa3e587c8bd9e1a2b6e2fbfa91bc9279
gordenyong <yongkunghee@hotmail.com>
2022-03-18 06:58:41 +0800

    view auction

--- client/js/components/auction_list.js
+++ client/js/components/auction_list.js
@@ -41,20 +41,27 @@ function renderAuctions() {
       document.querySelector("#active-auctions").innerHTML = auctions
         .map(
           (auction) => `
-      <section class='auction' data-id="${
-        auction.id
-      }" onClick="auctionBiddingPage(${auction.id})">
-          <div>
-            <img src="${auction.image_url}" alt="">
-          </div>   
-          <header>
-              <h2>${auction.item_name}</h2>
-              <h3>$${auction.current_price}</h3>
-              <h3>Ending in: ${auction.end_time - auction.start_time}</h3>
-          </header>
-      </section>
-  `
+        <section class='auction' data-id="${
+          auction.id
+        }" onClick="auctionBiddingPage(${auction.id})">
+            <div>
+              <img src="${auction.image_url}" alt="">
+            </div>   
+            <header>
+                <h2>${auction.item_name}</h2>
+                <h3>$${auction.current_price}</h3>
+                <h3>Ending in: ${auction.end_time - auction.start_time}</h3>
+            </header>
+        </section>
+    `
         )
         .join("");
     });
+}
+
+function auctionBiddingPage(auctionID) {
+  axios
+    .get(`/api/auctions/${auctionID}`)
+    .then((res) => res.data)
+    .then((auction) => renderAuction(auction));
 }
